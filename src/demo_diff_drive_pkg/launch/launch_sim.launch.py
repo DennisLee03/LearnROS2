7c02241d4c5b6e7bb192dd9b00f84ff30f74727a
dennis <e64112287@gs.ncku.edu.tq>
2025-08-20 15:09:11 +0800

    review diffdrive

--- src/demo_diff_drive_pkg/launch/launch_sim.launch.py
+++ src/demo_diff_drive_pkg/launch/launch_sim.launch.py
@@ -5,7 +5,6 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
@@ -13,35 +12,28 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    package_name='demo_diff_drive_pkg'
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='demo_diff_drive_pkg' #<--- CHANGE ME
-
+    # ros2 run robot_state_publisher robot_state_publisher --ros-args -p robot_description:="$(xacro /path/to/robot.urdf.xacro sim_mode:=true)"
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                os.path.join(get_package_share_directory(package_name),'launch','rsp.launch.py'), 
+                launch_arguments=[('use_sim_time', 'true')]
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # ros2 launch gazebo_ros gazebo.launch.py
+    gazebo_classic = IncludeLaunchDescription(
+        os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+    # ros2 run gazebo_ros spawn_entity.py -topic robot_description -entity my_bot
+    # TODO: change to Ignition
+    spawn_entity = Node(package='gazebo_ros', 
+                        executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
                         output='screen')
 
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
-        gazebo,
+        gazebo_classic,
         spawn_entity,
     ])
--- src/demo_diff_drive_pkg/launch/rsp.launch.py
+++ src/demo_diff_drive_pkg/launch/rsp.launch.py
@@ -1,26 +1,21 @@
-# join dirs
 import os
-from launch.substitutions import PathJoinSubstitution
-
-# get share dir path
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.substitutions import FindPackageShare
 
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
-import xacro
-
 def generate_launch_description():
     
+    # declare this launch file's arguments
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # process urdf
-    pkg_path = os.path.join(get_package_share_directory('demo_diff_drive_pkg')) # FindPackageShare().__str__()
+    pkg_path = os.path.join(get_package_share_directory('demo_diff_drive_pkg'))
     xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+    # description:="$(xacro path/to/robot.urdf.xacro)"
     robot_description_config = Command([
         'xacro ', xacro_file, 
         ' use_ros2_control:=', use_ros2_control, 
@@ -28,12 +23,12 @@ def generate_launch_description():
     ])
 
     # create a robot_state_publisher
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    params = [{'robot_description': robot_description_config, 'use_sim_time': use_sim_time}]
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=params
     )
 
     return LaunchDescription([
